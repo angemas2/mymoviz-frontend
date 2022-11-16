0ee887c4dbbecf37033f6b33795af3691ec90d3d
Angélique <angelique.massolo@gmail.com>
2022-11-16 12:01:49 +0100

    fronturl

--- components/Home.js
+++ components/Home.js
@@ -12,7 +12,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/movies")
+    fetch("https://mymoviz-backend-eight.vercel.app/movies")
       .then((response) => response.json())
       .then((data) => {
         setMoviesData(data.movies);
